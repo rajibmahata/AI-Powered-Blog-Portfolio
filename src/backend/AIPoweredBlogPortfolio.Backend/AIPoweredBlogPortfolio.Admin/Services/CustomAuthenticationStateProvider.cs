462e784043a4e09ab1840451b4f054e72a82c485
rajibmahata <rajibmahata143@outlook.com>
2025-02-16 20:20:12 +0530

    added class and razor file

---
+++ src/backend/AIPoweredBlogPortfolio.Backend/AIPoweredBlogPortfolio.Admin/Services/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace AIPoweredBlogPortfolio.Admin.Services
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly AuthenticationState anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(anonymous);
+        }
+
+        public Task MarkUserAsAuthenticated(string username)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, username) }, "apiauth"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+            return Task.CompletedTask;
+        }
+
+        public Task MarkUserAsLoggedOut()
+        {
+            var authState = Task.FromResult(anonymous);
+            NotifyAuthenticationStateChanged(authState);
+            return Task.CompletedTask;
+        }
+    }
+}
